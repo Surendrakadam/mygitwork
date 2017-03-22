c8b85da76265cdc7b3efca8f5df9449449729bd5
surendrakadam <surendrak@identlogic.com>
2017-03-22 15:40:18 +0530

    ID part and log file with timestamp

--- MkeKey.c
+++ MkeKey.c
@@ -1,4 +1,4 @@
-/* cl MkeKey.c stssan3cl.lib
+/*
 
  Creates Key file sssrrrr.oke from tagged file sssrrrr.tag.
 */
@@ -9,7 +9,7 @@
 #include "ssan3cl.h"                                            // Dedupe header
 #include <getopt.h>
 #include <unistd.h>
-#include <time.h>
+#include <time.h>                                               // Timer header
 
 #define rangeof(arr)  (sizeof(arr)/sizeof((arr)[0]))
 
@@ -73,17 +73,27 @@
   char cpy_dir_out_fl_ext[500] ;                                // copy of Directory with output file
   char cpy_dir_log_fl_ext[500] ;                                // copy of Directory with log file
 
-  char *ID = "";                                                // Substring ID from the current record
-  char *ptr_id = "" ;                                           // Point to the string *ID* in current record till end of the current record
-  char *search = "*Id*";                                        // Search word *Id* in current record
-  int   id_start_pos = 0;                                       // Starting position of *Id*
+  char *ID        = "";                                         // Substring ID from the current record
+  char *ptr_id    = "" ;                                        // Point to the string *ID* in current record till end of the current record
+  char *search    = "*Id*";                                     // Search word *Id* in current record
   char  character = '*';                                        // Character * asterisk
-  int   pos_afr_id = 0;                                         // Position after *Id* 
-  int   asterisk_start_pos = 0;                                 // After *ID* first * position
-  int   id_len = 0;                                             // Length of the *Id* i.e 4
-  int   frt_ast_pos = 0 ;                                       // First asterisk position after *Id*
+  
+  int   id_start_pos       = 0 ;                                // Starting position of *Id*
+  int   pos_afr_id         = 0 ;                                // Position after *Id*
+  int   asterisk_start_pos = 0 ;                                // After *ID* first * position
+  int   id_len             = 0 ;                                // Length of the *Id* i.e 4
+  int   frt_ast_pos        = 0 ;                                // First asterisk position after *Id*
+  
+  // Time variables
+  int YYYY = 0;                                                 // Year
+  int MM   = 0;                                                 // Month
+  int DD   = 0;                                                 // Date
+  int H24  = 0;                                                 // Hour-24  
+  int MI   = 0;                                                 // Minute
+  int SS   = 0;                                                 // Seconds
+  char sss_rrrr_timestamp[500];
 
-static long test_ssa_open (
+static long s_test_ssa_open (
   long    sockh ,
   long    *session_id ,
   char    *sysName ,
@@ -110,17 +120,20 @@ static long test_ssa_open (
     SSA_SI_RSP_SZ ,
     ssa_msg ,
     SSA_SI_SSA_MSG_SZ) ;
+
   if (rc < 0) {
     fprintf ( log_fopen_status, "rc               : %ld\n" , rc) ;
     rc = 1 ;
     goto ret ;
   }
+
   if (rsp_code[0] != '0' && *session_id == -1) {
     fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
     fprintf ( log_fopen_status, "ssa_msg          : %s\n" , ssa_msg) ;
     rc = 1 ;
     goto ret ;
   }
+
   fprintf ( log_fopen_status, "\n") ;
   fprintf ( log_fopen_status, "Session Id       : %ld\n" , *session_id) ;
   fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
@@ -132,8 +145,11 @@ ret:
   return (rc) ;
 }
 
+/**********************************************************************
+ End of subroutine s_test_ssa_open                                      *
+**********************************************************************/
 
-static long test_ssa_get_keys (
+static long s_test_ssa_get_keys (
 
   long    sockh ,
   long    *session_id ,
@@ -142,9 +158,9 @@ static long test_ssa_get_keys (
   char    *controls ,
   char    *record ,
   long    recordLength ,
-  char    *recordEncType 
+  char    *recordEncType
 ) {
-  int   i ;
+  int     i ;
   long    rc ;
   char    *keys_array[SSA_SI_MAX_KEYS] ;
   char    keys_data[SSA_SI_MAX_KEYS*SSA_SI_KEY_SZ] ;
@@ -156,7 +172,7 @@ static long test_ssa_get_keys (
   for (i = 0 ; i < (int)rangeof (keys_array) ; ++i)
     keys_array[i] = keys_data + i * SSA_SI_KEY_SZ ;
 
-  fprintf ( log_fopen_status, "------ ssan3_get_keys_encoded ------\n") ;  //-- suppress ... ???
+  fprintf ( log_fopen_status, "------ ssan3_get_keys_encoded ------\n") ;
   fprintf ( log_fopen_status, "Session Id       : %ld\n" , *session_id) ;
   fprintf ( log_fopen_status, "System           : %s\n" , sysName) ;
   fprintf ( log_fopen_status, "Population       : %s\n" , population) ;
@@ -190,12 +206,14 @@ static long test_ssa_get_keys (
     rc = 1 ;
     goto ret ;
   }
+  
   if (rsp_code[0] != '0') {
     fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
     fprintf ( log_fopen_status, "ssa_msg          : %s\n" , ssa_msg) ;
     rc = -1 ;
     goto ret ;
   }
+  
   fprintf ( log_fopen_status, "\n") ;
   fprintf ( log_fopen_status, "Session Id       : %ld\n" , *session_id) ;
   fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
@@ -203,13 +221,13 @@ static long test_ssa_get_keys (
   fprintf ( log_fopen_status, "Count            : %ld\n" , num) ;
   //printf ("------ keys ------\n") ;
   ++rec_number ;
-  fprintf(output_fopen_status ,"\n%d :",rec_number) ;
+  //fprintf(output_fopen_status ,"\n%d :",rec_number) ;
   for (i = 0 ; i < num ; ++i) {
     p = keys_array[i] ;
-    fprintf ( output_fopen_status ,"\n'%.*s'" , SSA_SI_KEY_SZ , p) ;
+    fprintf ( output_fopen_status ,"\n%.*s" , SSA_SI_KEY_SZ , p) ;
     //printf ("%3d '%.*s'\n" , i+1 , SSA_SI_KEY_SZ , p) ;  //-- OUTPUT
   }
-  //printf ("\n") ;
+  //fprintf(output_fopen_status ,"\n") ;
   rc = 0 ;
   goto ret ;
 
@@ -217,12 +235,17 @@ ret:
   return (rc) ;
 }
 
-static long test_ssa_close (
+/**********************************************************************
+ End of subroutine s_test_ssa_get_keys                                  *
+**********************************************************************/
+
+static long s_test_ssa_close (
+
   long    sockh ,
   long    *session_id ,
   char    *sysName ,
   char    *population ,
-  char    *controls 
+  char    *controls
 ) {
   long    rc ;
   char    rsp_code[SSA_SI_RSP_SZ] ;
@@ -243,17 +266,20 @@ static long test_ssa_close (
     SSA_SI_RSP_SZ ,
     ssa_msg ,
     SSA_SI_SSA_MSG_SZ) ;
+    
   if (rc < 0) {
     fprintf ( log_fopen_status, "rc               : %ld\n" , rc) ;
     rc = 1 ;
     goto ret ;
   }
+  
   if (rsp_code[0] != '0') {
     fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
     fprintf ( log_fopen_status, "ssa_msg          : %s\n" , ssa_msg) ;
     rc = 1 ;
     goto ret ;
   }
+  
   fprintf ( log_fopen_status, "\n") ;
   fprintf ( log_fopen_status, "Session Id       : %ld\n" , *session_id) ;
   fprintf ( log_fopen_status, "rsp_code         : %s\n" , rsp_code) ;
@@ -265,6 +291,10 @@ ret:
   return (rc) ;
 }
 
+/**********************************************************************
+ End of subroutine s_test_ssa_close                                     *
+**********************************************************************/
+
 static void doExit ( char *func) {
   printf ("Error occurred in '%s'\n" , func) ;
   exit (EXIT_FAILURE) ;
@@ -273,19 +303,20 @@ static void doExit ( char *func) {
 static void s_mkeKey_open () {
 
     // Establish a session.
-    rc = test_ssa_open (
+    rc = s_test_ssa_open (
       sockh ,
       &session_id ,
       "default" ,
       p_population ,
       "") ;
+      
     if (0 != rc)
-      doExit ("test_ssa_open") ;
+      doExit ("s_test_ssa_open") ;
 
 }
 
 /**********************************************************************
- End of subroutine s_mkeKey_open                                    *
+ End of subroutine s_mkeKey_open                                      *
 **********************************************************************/
 
 static void s_mkeKey_getKey (
@@ -303,7 +334,7 @@ static void s_mkeKey_getKey (
 
   for( i = 0 ; i <= 2 ; i++ ) {
     rc =
-      test_ssa_get_keys (
+      s_test_ssa_get_keys (
         sockh ,
         &session_id ,
         "default" ,
@@ -313,10 +344,11 @@ static void s_mkeKey_getKey (
         cur_rec_len ,
         "TEXT"
       ) ;
+      
     fprintf ( output_fopen_status , "%s" , ky_fld_ky_lvl [ i ] ) ;
     fprintf (output_fopen_status,"%s",ID) ;
     if ( 0 != rc )
-      doExit ( "test_ssa_get_keys" ) ;
+      doExit ( "s_test_ssa_get_keys" ) ;
   }
 
 }
@@ -326,7 +358,7 @@ static void s_mkeKey_getKey (
 **********************************************************************/
 
 static void s_mkeKey_close ( ) {
-  rc = test_ssa_close (
+  rc = s_test_ssa_close (
     sockh ,
     &session_id ,
     "default" ,
@@ -334,7 +366,7 @@ static void s_mkeKey_close ( ) {
     "") ;
 
   if (0 != rc)
-    doExit ("test_ssa_close") ;
+    doExit ("s_test_ssa_close") ;
 }
 
 void print_usage() {
@@ -346,6 +378,18 @@ void print_usage() {
 **********************************************************************/
 
 static void s_getParameter(int argc , char *argv[]) {
+
+  time_t my_time ;                                              // Time to get current time
+  struct tm * timeinfo ;
+  time (&my_time) ;
+  timeinfo = localtime (&my_time);
+  
+  YYYY = timeinfo->tm_year+1900;
+  MM   = timeinfo->tm_mon+1;
+  DD   = timeinfo->tm_mday;
+  H24  = timeinfo->tm_hour;
+  MI   = timeinfo->tm_min;
+  SS   = timeinfo->tm_sec;
 
   while ( ( option = getopt ( argc , argv , "d:r:p:i:o:l:" ) ) != -1 ) {
     switch (option) {
@@ -441,31 +485,34 @@ static void s_getParameter(int argc , char *argv[]) {
 
   sss_rrrr = strcat(p_data_set ,p_run_time) ;                   // Concatenate data set number and run time number
 
-  strcpy( sss_rrrr_in_file , sss_rrrr ) ;                         // Copy value of sss_rrrr to sss_rrrr_in_file
-  strcpy( sss_rrrr_out_file , sss_rrrr ) ;                        // Copy value of sss_rrrr to sss_rrrr_out_file
-  strcpy( sss_rrrr_log_file , sss_rrrr ) ;                        // Copy value of sss_rrrr to sss_rrrr_log_file
+  strcpy( sss_rrrr_in_file , sss_rrrr ) ;                       // Copy value of sss_rrrr to sss_rrrr_in_file
+  strcpy( sss_rrrr_out_file , sss_rrrr ) ;                      // Copy value of sss_rrrr to sss_rrrr_out_file
+  strcpy( sss_rrrr_log_file , sss_rrrr ) ;                      // Copy value of sss_rrrr to sss_rrrr_log_file
+  
+  // Log_file concatenate with timestamp
+  sprintf(sss_rrrr_timestamp,"%s_%d_%d_%d_%d_%d_%d",sss_rrrr_log_file,YYYY,MM,DD,H24,MI,SS);
 
-  infname = strcat( sss_rrrr_in_file ,".tag" );                   // Input file name with extension
-  ofname  = strcat( sss_rrrr_out_file,".oke" ) ;                 // Output file name with extension
-  logfname= strcat( sss_rrrr_log_file,".log" );                  // Log file name with extension
+  infname  = strcat( sss_rrrr_in_file ,".tag" ) ;               // Input file name with extension
+  ofname   = strcat( sss_rrrr_out_file,".oke" ) ;               // Output file name with extension
+  logfname = strcat( sss_rrrr_timestamp,".log" ) ;               // Log file name with extension
 
-  strcpy( cpy_infname,infname );                                // Copy of Input file name
-  strcpy( cpy_ofname,ofname );                                  // Copy of output file name
-  strcpy( cpy_logfname,logfname );                              // Copy of log file name
+  strcpy( cpy_infname,infname ) ;                               // Copy of Input file name
+  strcpy( cpy_ofname,ofname ) ;                                 // Copy of output file name
+  strcpy( cpy_logfname,logfname ) ;                             // Copy of log file name
 
   // Concatenate Directory path with Input file name
   dir_in_fl_ext = strcat( input_file_directory ,cpy_infname );
-  strcpy(cpy_dir_in_fl_ext,dir_in_fl_ext);
+  strcpy(cpy_dir_in_fl_ext,dir_in_fl_ext) ;
 
   // Concatenate Directory path with output file name
   dir_out_fl_ext = strcat( output_file_directory, cpy_ofname );
-  strcpy(cpy_dir_out_fl_ext,dir_out_fl_ext);
+  strcpy(cpy_dir_out_fl_ext,dir_out_fl_ext) ;
 
   // Concatenate Directory path with log file name
   dir_log_fl_ext = strcat( log_file_direcory, cpy_logfname );
-  strcpy(cpy_dir_log_fl_ext,dir_log_fl_ext);
+  strcpy(cpy_dir_log_fl_ext,dir_log_fl_ext) ;
 
-
+  
   input_fopen_status   = fopen ( cpy_dir_in_fl_ext, "r" ) ;     // Open and read input file
   output_fopen_status  = fopen ( cpy_dir_out_fl_ext, "w" ) ;    // Open and write output file
   log_fopen_status     = fopen ( cpy_dir_log_fl_ext , "w" ) ;   // Open and write log file
@@ -492,19 +539,17 @@ static void s_getParameter(int argc , char *argv[]) {
 
 int main ( int argc , char *argv[] ) {
 
-
   id_len = strlen( search ) ;                                   // Length of search string
   s_getParameter ( argc , argv ) ;                              // Subroutine to get parameter
   t = clock() ;                                                 // Start time
 
   s_mkeKey_open() ;                                             // subroutine to open ssa connection
 
-
   // Call ssan3_get_keys
 
   // Read a input file line by line
   while( fgets (  current_rec , sizeof (current_rec) , input_fopen_status  ) ) {
-    
+
       cur_rec_len = strlen( current_rec ) ;
       if ( cur_rec_len > 0 && current_rec[cur_rec_len-1] == '\n' ) {
         current_rec[--cur_rec_len] = '\0' ;
@@ -514,15 +559,15 @@ int main ( int argc , char *argv[] ) {
 
         ptr_id       = strstr ( current_rec , search ) ;        // Search *ID* in current record capture string from *ID* to till end of the string
         id_start_pos = (ptr_id - current_rec) ;                 // Starting position of *ID*
-        pos_afr_id   = ( id_start_pos + id_len ) ;              // Position after *Id* 
-        
+        pos_afr_id   = ( id_start_pos + id_len ) ;              // Position after *Id*
+
         for( i = pos_afr_id ; i < cur_rec_len; i++ ) {
           if( current_rec[i] == character ) {
               asterisk_start_pos = i ;                          // After *ID* first * position
               break ;
           }
         }
-        
+
         frt_ast_pos = ( asterisk_start_pos - pos_afr_id ) ;     // First asterisk position after *Id*
         strncpy( ID,current_rec + pos_afr_id, frt_ast_pos ) ;   // Substring Id
 
@@ -530,7 +575,7 @@ int main ( int argc , char *argv[] ) {
           char *PL = "FIELD=Person_Name KEY_LEVEL=Limited" ;
           char *PS = "FIELD=Person_Name KEY_LEVEL=Standard" ;
           char *PE = "FIELD=Person_Name KEY_LEVEL=Extended" ;
-          
+
           char *abv_PLim   = "PM" ;
           char *abv_PStand = "PS" ;
           char *abv_PExt   = "PX" ;
@@ -542,11 +587,11 @@ int main ( int argc , char *argv[] ) {
           char *OL = "FIELD=Organization_Name KEY_LEVEL=Limited" ;
           char *OS = "FIELD=Organization_Name KEY_LEVEL=Standard" ;
           char *OE = "FIELD=Organization_Name KEY_LEVEL=Extended" ;
-          
+
           char *abv_OLim   = "OM" ;
           char *abv_OStand = "OS" ;
           char *abv_OExt   = "OX" ;
-          
+
           // Call
           s_mkeKey_getKey( OL , OS , OE , abv_OLim , abv_OStand , abv_OExt,ID ) ;
         }
@@ -555,18 +600,15 @@ int main ( int argc , char *argv[] ) {
           char *AL = "FIELD=Address_Part1 KEY_LEVEL=Limited" ;
           char *AS = "FIELD=Address_Part1 KEY_LEVEL=Standard" ;
           char *AE = "FIELD=Address_Part1 KEY_LEVEL=Extended" ;
-          
+
           char *abv_ALim   = "1M" ;
           char *abv_AStand = "1S" ;
           char *abv_AExt   = "1X" ;
-          
+
           s_mkeKey_getKey( AL , AS , AE , abv_ALim , abv_AStand , abv_AExt ,ID ) ;
         }
      }
-
-
   }
-
 
   s_mkeKey_close() ;                                            // subroutine to close ssa connection
 
